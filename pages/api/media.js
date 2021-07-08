fece26e77ef5749ef47ab410a7a1644ae8516fb7
copy-ninja1 <emmajnr1000@gmail.com>
2021-07-08 02:57:03 +0100

    fix: change api key

--- pages/api/media.js
+++ pages/api/media.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     // let _query = !req.query && !!req.query.q ? req.query.q : 'trending%20songs'
     // console.log({ q: typeof req.query.q })
     console.log({ r: req.query.q })
-    let data = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}&type=video&key=AIzaSyCEQJwLB3oFqEdtSU7FhryuYRhslybWLno`)
+    let data = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}&type=video&key=AIzaSyDRmmq5UNzg36OzRCV3Otz1NvIBdCZjCWw`)
         .then(response => response.json())
         .then(data => data);
     if (data.items) {
