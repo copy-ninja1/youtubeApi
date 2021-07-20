11abb06bcb326fefed4a754ce64c503e795b01ca
copy-ninja1 <emmajnr1000@gmail.com>
2021-07-20 23:05:00 +0100

    test

--- pages/api/media.js
+++ pages/api/media.js
@@ -1,33 +1,29 @@
 const cheerio = require('cheerio')
 export default async function handler(req, res) {
-    //  // console.log({ query: req.query })
-    //  // let _query = !req.query && !!req.query.q ? req.query.q : 'trending%20songs'
-    //  // console.log({ q: typeof req.query.q })
+    console.log({ r: req.query.q })
+    // let data = await fetch(`https://mail.naijagreen.com.ng/s/${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}`)
+    //     .then(response => response.text())
+    //     .then(data => data).catch((error) => {
+    //         return res.status(500).send(error.message || error);
+    //     })
 
-    // console.log({ r: req.query.q })
-    let data = await fetch(`https://mail.naijagreen.com.ng/s/${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}`)
-        .then(response => response.text())
-        .then(data => data).catch((error) => {
-            return res.status(500).send(error.message || error);
-        })
+    // const $ = cheerio.load(data);
+    // var _videos = []
 
-    const $ = cheerio.load(data);
-    var _videos = []
+    // $('div.card.card-cascade').each(function (i, element) {
+    //     var $ele = $(element)
+    //     var id = $ele.find('a').attr('href').split('/')[1]
+    //     var img = $ele.find('img')
+    //     console.log({ element: img.attr('alt') })
 
-    $('div.card.card-cascade').each(function (i, element) {
-        var $ele = $(element)
-        var id = $ele.find('a').attr('href').split('/')[1]
-        var img = $ele.find('img')
-        console.log({ element: img.attr('alt') })
-
-        _videos.push({
-            id: i + 1,
-            uid: id,
-            title: img.attr('alt'),
-            imageSrc: img.attr('src')
-        })
-    });
-    res.status(200).json({ videos: _videos })
+    //     _videos.push({
+    //         id: i + 1,
+    //         uid: id,
+    //         title: img.attr('alt'),
+    //         imageSrc: img.attr('src')
+    //     })
+    // });
+    // res.status(200).json({ videos: _videos })
 
     // const html = await fethHtml(`https://mail.naijagreen.com.ng/s/${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}/`);
     // const $ = cheerio.load(html);
@@ -44,6 +40,39 @@ export default async function handler(req, res) {
     //     })
     // });
     // res.status(200).json({ videos: _videos })
+    // 
+    // 
+
+
+
+    try { // 4
+        const response = await fetch(`https://mail.naijagreen.com.ng/s/${req.query.q != 'undefined' ? req.query.q : 'Wizkid, Davido, Mr.Eazi, Burna Boy ,2baba,Naira Marley,Sinach,Flavor'}`)
+        const htmlString = await response.text()
+        const $ = cheerio.load(htmlString)
+        var _videos = []
+
+        $('div.card.card-cascade').each(function (i, element) {
+            var $ele = $(element)
+            var id = $ele.find('a').attr('href').split('/')[1]
+            var img = $ele.find('img')
+            console.log({ element: img.attr('alt') })
+
+            _videos.push({
+                id: i + 1,
+                uid: id,
+                title: img.attr('alt'),
+                imageSrc: img.attr('src')
+            })
+        });
+        res.statusCode = 200
+        return res.json({ videos: _videos })
+    } catch (e) { // 5
+        res.statusCode = 500
+        return res.json({
+            error: e,
+        })
+    }
+    // 
 
 }
 
